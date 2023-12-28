91776e6723121f3056b3c0850d89e0f3ebe58d39
sunfloweboi <65011381@kmitl.ac.th>
2023-12-28 13:54:03 +0700

    3.1 remake
    remade 3.1

--- towerofhanoi.py
+++ towerofhanoi.py
@@ -7,33 +7,41 @@ class Disk:
         self.dypos = y
         self.dheight = height
         self.dwidth = width
+        self.t = t.Turtle()
     
     def showdisk(self):
-        t.penup()
-        t.goto(self.dxpos, self.dypos)
-        t.setheading(0)
-        t.pendown()
-        t.fillcolor("red")
-        t.begin_fill()
-        for i in range(2):
-            t.forward(self.dwidth/2)
-            t.left(90)
-            t.forward(self.dheight)
-            t.left(90)
-        t.end_fill()
-        t.penup()
-        t.setheading(0)
-        t.goto(self.dxpos, self.dypos)
+        self.t.penup()
+        self.t.goto(self.dxpos, self.dypos)
+        self.t.setheading(0)
+        
+        
+        self.t.fillcolor("brown")
+        self.t.begin_fill()
+        self.t.pendown()
+        self.t.forward(self.dwidth/2)
+        self.t.left(90)
+        self.t.forward(self.dheight)
+        self.t.left(90)
+        self.t.forward(self.dwidth)
+        self.t.left(90)
+        self.t.forward(self.dheight)
+        self.t.left(90)
+        self.t.forward(self.dwidth/2)
+        self.t.end_fill()
+        self.t.penup()
+        self.t.setheading(0)
+        self.t.goto(self.dxpos, self.dypos)
     
     def newpos(self, x, y):
         self.dxpos = x
         self.dypos = y
     
     def cleardisk(self):
-        t.setheading(0) 
-        t.goto(self.dxpos, self.dypos) 
-        t.clear()  
-        t.setheading(0)  
-        t.goto(self.dxpos, self.dypos)  
+        self.t.setheading(0) 
+        self.t.goto(self.dxpos, self.dypos) 
+        self.t.clear()  
+        self.t.setheading(0)  
+        self.t.goto(self.dxpos, self.dypos) 
         
+
         
